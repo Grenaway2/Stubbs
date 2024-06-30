7f879a45e6ef9509195f48a4eb514fd5590b11da
Whitney Grenaway <wmgrenaway@search1media.com>
2024-06-30 07:41:53 -0400

    Fix unescaped characters and structure in about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ const About = () => {
       </div>
       <div className="about-content">
         <p>
-          Working labor jobs back to back, doing more work than my other coworkers but still getting paid the same, just wasn't it for me. I honestly just wanted to be a better father and be there for my family, but I didn't have the time nor the money to do so. I took a risk and instead of loading the trucks that would pull up to my old warehouse, I bought a truck of my own, and my life changed.
+          Working labor jobs back to back, doing more work than my other coworkers but still getting paid the same, just wasn&apos;t it for me. I honestly just wanted to be a better father and be there for my family, but I didn&apos;t have the time nor the money to do so. I took a risk and instead of loading the trucks that would pull up to my old warehouse, I bought a truck of my own, and my life changed.
         </p>
         <p>
           Starting out was tough, but I had the freedom to work on my own schedule and cut costs where I saw fit to run my business. In the book, I will tell you a step-by-step way to get into the box truck game, also about the mistakes I made, and share some personal experiences that cost me a lot of time that you are able to learn from. I still make mistakes to this day, so it is a never-ending process of learning.
