68db06204bbdea41b0a1cc4e89266c0c0ab53dfa
Whitney Grenaway <wmgrenaway@search1media.com>
2024-07-09 08:13:13 -0400

    Updated all changes

--- src/pages/pictures.js
+++ src/pages/pictures.js
@@ -7,16 +7,7 @@ const Pictures = () => {
       <h1 className="pictures-title">Pictures</h1>
       <div className="pictures-grid">
         <div className="picture-item">
-          <Image src="/images/picture1.jpg" alt="Picture 1" width={300} height={200} />
-        </div>
-        <div className="picture-item">
-          <Image src="/images/picture2.jpg" alt="Picture 2" width={300} height={200} />
-        </div>
-        <div className="picture-item">
           <Image src="/images/picture3.jpg" alt="Picture 3" width={300} height={200} />
-        </div>
-        <div className="picture-item">
-          <Image src="/images/picture4.jpg" alt="Picture 4" width={300} height={200} />
         </div>
         <div className="picture-item">
           <Image src="/images/picture5.jpg" alt="Picture 5" width={300} height={200} />
@@ -26,9 +17,6 @@ const Pictures = () => {
         </div>
         <div className="picture-item">
           <Image src="/images/picture7.jpg" alt="Picture 7" width={300} height={200} />
-        </div>
-        <div className="picture-item">
-          <Image src="/images/picture8.jpg" alt="Picture 8" width={300} height={200} />
         </div>
       </div>
     </div>
